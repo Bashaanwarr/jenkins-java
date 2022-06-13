bfe8c8fb82030842b4f8b033cb40722792cd080b
anwar <anwarbasha880@gmail.com>
2022-06-13 15:15:45 +0530

    javacommit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloWorld 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello World!, WELCOME to Java code files process"); 
+        
+        int n1 = 100, n2 = 150, sum;  
+        sum = n1 + n2;  
+        System.out.println("The sum of numbers is: "+sum);  
+    } 
+} 
